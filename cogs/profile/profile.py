6d8f02b5100a4b233d7e64f4d24dd58d0a6e8fb8
Arjun Ladha <tigerninja2234@gmail.com>
2022-07-17 19:09:49 +0530

    chore: add LICENSE and license all python files

--- cogs/profile/profile.py
+++ cogs/profile/profile.py
@@ -20,31 +20,30 @@
 # SOFTWARE.
 
 import discord
-import json
-import os
+from discord import guild_only, slash_command
+from discord.ext import commands
+from discord.commands import slash_command, Option
 
-# Get configuration.json
-with open("configuration.json", "r") as config: 
-	data = json.load(config)
-	token = data["token"]
-	owner_id = data["owner_id"]
+from utils import *
 
-# Intents
-intents = discord.Intents.all()
-# The bot
-bot = discord.Bot(intents = intents, owner_id = owner_id)
 
-# Load cogs
-if __name__ == '__main__':
-	for folder in os.listdir("cogs"):
-		for filename in os.listdir(f"cogs/{folder}"):
-			if filename.endswith(".py"):
-				bot.load_extension(f"cogs.{filename[:-3]}")
-
-@bot.event
-async def on_ready():
-	print(f"We have logged in as {bot.user}")
-	print(discord.__version__)
-	await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=f"Cooking Food!"))
-
-bot.run(token)
+class Account(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"{self.__class__.__name__} Cog has been loaded")
+        
+    @guild_only()
+    @slash_command(
+        name='profile',
+        usage='/profile <member>',
+        description='View your or someone else\'s profile'
+    )
+    async def profile(self, ctx, member : Option(discord.member, required=False)=None):
+        if not member:
+            member = ctx.author
+            
+        if not check_acc(ctx.author.id):
+            return await ctx.respond("This user doesn't have a profile as they haven't played yet!")
